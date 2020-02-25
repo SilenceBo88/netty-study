1018c215967534b1e92b90119fede6b8b87ec2eb
zhaobo <zhaobo@cvte.com>
2020-02-25 21:57:18 +0800

    [add][项目初始化]

---
+++ src/main/java/cn/zb/study/io/IOClient.java
@@ -0,0 +1,31 @@
+package cn.zb.study.io;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * @Description: IO方式客户端
+ * @Author: zb
+ * @Date: 2020-02-25
+ */
+public class IOClient {
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 8000);
+                while (true) {
+                    try {
+                        socket.getOutputStream().write((new Date() + ": hello world").getBytes());
+                        Thread.sleep(2000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
